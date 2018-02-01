f13d2a52ab0dbef407e4844c6e36485cff5b3f57
Kunal Malhotra <kunal.malhotra1@ibm.com>
2018-02-01 17:50:06 +0400

    Setuped Routes

--- app.py
+++ app.py
@@ -1,7 +1,44 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/ca')
+def ca():
+	return render_template('ca.html')
+
+@app.route('/customs')
+def customs():
+	return render_template('customs.html')
+
+@app.route('/fa')
+def fa():
+	return render_template('fa.html')
+
+@app.route('/ihc')
+def ihc():
+	return render_template('ihc.html')
+
+@app.route('/mofa')
+def mofa():
+	return render_template('mofa.html')
+
+@app.route('/moh')
+def moh():
+	return render_template('moh.html')
+
+@app.route('/who')
+def who():
+	return render_template('who.html')
+
 if __name__ == '__main__':
-	app.run(debug = True)
+	app.debug = True
+	host = os.environ.get('IP','0.0.0.0')
+	port = int(os.environ.get('PORT','8080'))
+	app.run(host=host, port=port)
+
 
